2607b1f0fb2ba9c115b93f33f0178d63020effe6
Ashish maurya <62279600+ashishmaurya-crypto@users.noreply.github.com>
2022-01-03 10:12:46 +0530

    Updates

---
+++ precache-manifest.929806f4cccd455f5a9526b3d22da3a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4f0f6ec792841bbe940",
+    "url": "/robofriends/static/css/main.de051fa3.chunk.css"
+  },
+  {
+    "revision": "f4f0f6ec792841bbe940",
+    "url": "/robofriends/static/js/main.f4f0f6ec.chunk.js"
+  },
+  {
+    "revision": "2b7bc563f7aa9f8b7b19",
+    "url": "/robofriends/static/css/1.eaf966da.chunk.css"
+  },
+  {
+    "revision": "2b7bc563f7aa9f8b7b19",
+    "url": "/robofriends/static/js/1.2b7bc563.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "b959411a4c99aa9f7e2706452640c9cc",
+    "url": "/robofriends/index.html"
+  }
+];
